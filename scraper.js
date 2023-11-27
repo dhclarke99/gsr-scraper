6b47829db2112eb712107a5b9e9c8abe1d36ab90
dhclarke99 <dhclarke99@gmail.com>
2023-11-26 18:44:11 -0800

    adjusted code to scrape all pdfs..

--- scraper.js
+++ scraper.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const pdf = require('pdf-parse');
 
 async function parsePDF(filePath) {
@@ -6,20 +7,19 @@ async function parsePDF(filePath) {
     const data = await pdf(dataBuffer);
 
     const schoolPattern = /Cohorts: (.+?)$/m;
-    const yearPattern = /(\d{4} - \d{4}) Cohorts/;
-    // Revised regex pattern to handle sports names better
+    const yearPattern = /(\d{4}) - (\d{4}) Cohorts/;
     const sportPattern = /([A-Za-z\/\.\s]+)(\d{2,3})(\d{2})/g;
 
     const schoolMatch = data.text.match(schoolPattern);
     const yearMatch = data.text.match(yearPattern);
     const school = schoolMatch ? schoolMatch[1] : 'Unknown School';
-    const year = yearMatch ? yearMatch[1] : 'Unknown Year';
+    // Modified to only include the last year in the range
+    const year = yearMatch ? yearMatch[2] : 'Unknown Year';
 
     const sections = data.text.split("Women's Sports");
     const menSportsText = sections[0];
-    const womenSportsText = "Women's Sports" + (sections[1] || ''); // Safeguard against missing women's section
+    const womenSportsText = "Women's Sports" + (sections[1] || '');
 
-    // Function to extract and format sports data
     const extractSportsData = (text) => {
         const sportsData = [];
         let match;
@@ -47,7 +47,26 @@ async function parsePDF(filePath) {
 }
 
 // Example usage
-const filePath = './pdfs/ASU GSR 2006-09.pdf';
-parsePDF(filePath).then(data => {
-    console.log(JSON.stringify(data, null, 2));
-});
+// Function to process all PDFs in a directory
+async function processPDFs(directoryPath) {
+    // Read the contents of the directory
+    const files = fs.readdirSync(directoryPath);
+    // Filter for .pdf files
+    const pdfFiles = files.filter(file => file.endsWith('.pdf'));
+  
+    // Process each PDF file
+    for (const file of pdfFiles) {
+      const filePath = path.join(directoryPath, file);
+      try {
+        const data = await parsePDF(filePath);
+        console.log(`Data for ${file}:`);
+        console.log(JSON.stringify(data, null, 2));
+      } catch (error) {
+        console.error(`Error processing ${file}:`, error);
+      }
+    }
+  }
+  
+  // Example usage: specify the directory containing the PDF files
+  const pdfDirectoryPath = './pdfs';
+  processPDFs(pdfDirectoryPath).catch(console.error);
